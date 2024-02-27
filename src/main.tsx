99db3b0900af83fb80000afde3d4b53dc1846f1c
Rustik <chuckis94@gmail.com>
2024-02-27 11:13:59 +0200

    'Make block Uppercase' functional

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,34 @@ import App from "./App";
 import "./index.css";
 
 import { logseq as PL } from "../package.json";
+import { IHookEvent } from "@logseq/libs/dist/LSPlugin";
 
 // @ts-expect-error
 const css = (t, ...args) => String.raw(t, ...args);
 
 const pluginId = PL.id;
 
-function main() {
-  console.info(`#${pluginId}: MAIN`);
+
+const main = async () => {
+
+  console.log('Plugin loaded!')
+
+  logseq.Editor.registerBlockContextMenuItem('Make block Uppercase',
+     async (e) => {
+      const blockUUID = e.uuid
+      // 1 get the content of the block
+      const currentBlock: any = await logseq.Editor.getBlock(blockUUID) // TIL AWAIT :D
+      // console.log(e.uuid)
+      // 2 change content to uppercase
+      const currentBlockText = currentBlock.content
+      console.log(currentBlockText)
+      const uppercaseText: string = currentBlockText?.toUpperCase()
+      console.log(uppercaseText)
+      // 3 save the block
+      logseq.Editor.updateBlock(blockUUID, uppercaseText)
+    
+  })
+  
   const root = ReactDOM.createRoot(document.getElementById("app")!);
 
   root.render(
@@ -22,39 +42,39 @@ function main() {
     </React.StrictMode>
   );
 
-  function createModel() {
-    return {
-      show() {
-        logseq.showMainUI();
-      },
-    };
-  }
+  // function createModel() {
+  //   return {
+  //     show() {
+  //       logseq.showMainUI();
+  //     },
+  //   };
+  // }
 
-  logseq.provideModel(createModel());
-  logseq.setMainUIInlineStyle({
-    zIndex: 11,
-  });
+  // logseq.provideModel(createModel());
+  // logseq.setMainUIInlineStyle({
+  //   zIndex: 11,
+  // });
 
-  const openIconName = "template-plugin-open";
+  // const openIconName = "template-plugin-open";
 
-  logseq.provideStyle(css`
-    .${openIconName} {
-      opacity: 0.55;
-      font-size: 20px;
-      margin-top: 4px;
-    }
+  // logseq.provideStyle(css`
+  //   .${openIconName} {
+  //     opacity: 0.55;
+  //     font-size: 20px;
+  //     margin-top: 4px;
+  //   }
 
-    .${openIconName}:hover {
-      opacity: 0.9;
-    }
-  `);
+  //   .${openIconName}:hover {
+  //     opacity: 0.9;
+  //   }
+  // `);
 
-  logseq.App.registerUIItem("toolbar", {
-    key: openIconName,
-    template: `
-      <div data-on-click="show" class="${openIconName}">⚙️</div>
-    `,
-  });
+  // logseq.App.registerUIItem("toolbar", {
+  //   key: openIconName,
+  //   template: `
+  //     <div data-on-click="show" class="${openIconName}">⚙️</div>
+  //   `,
+  // });
 }
 
 logseq.ready(main).catch(console.error);
